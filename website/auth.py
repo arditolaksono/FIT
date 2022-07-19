bc1fe1ffee1de3819434be91936df8b31ca0757a
arditolaksono <arditolaksono@gmail.com>
2022-07-19 22:45:36 +0700

    tambah method POST utk signup.html

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from django.shortcuts import render
+from flask import Blueprint,render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +13,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return 'Sign Up'
+    return render_template('daftar.html')
