71fc51cde3f25b00c7ff7880084b37a57704744e
arditolaksono <arditolaksono@gmail.com>
2022-07-20 22:21:53 +0700

    progres hari 3

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return 'Logout'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        namaLengkap = request.form.get('namaLengkap')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
     return render_template('daftar.html')
